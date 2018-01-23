78aa5a80ea96138b19dcce74747db0aaab6bdef6
Barathi Ganesh Hullathy Balakrishnan <barathiganesh.hb@gmail.com>
2018-01-23 21:47:55 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,8 +5,14 @@ setup(
     version='0.1',
     packages=['conrep-text'],
     url='https://github.com/BarathiGanesh-HB/conrep-text/',
+    download_url = 'https://github.com/BarathiGanesh-HB/conrep-text/archive/0.1.tar.gz',
     license='MIT',
     author='Barathi Ganesh HB',
     author_email='barathiganesh.hb@gmail.com',
-    description='Recursive Linear Transformer'
+    description='Recursive Linear Transformer',
+    install_requires=[
+        'numpy',
+        'nltk',
+        'gensim',
+    ],
 )
